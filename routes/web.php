153c3ab98869eb3567123a67f1b21d0b84b0f99c
kashifhussain146 <kashifhussain146@gmail.com>
2023-08-05 09:02:08 +0530

    testing

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/test-table', [HomeController::class, 'table'])->name('home');
 
+Route::get('/testing-table', [HomeController::class, 'table'])->name('home');
+
 Route::get('/assignment-help', [AssignmentController::class, 'index'])->name('assignment.help');
 Route::get('/assignment/help/{module_data_id}', [AssignmentController::class, 'assignmentDetails'])->name('assignment.help.details')->where(['slug' => '[a-z]+']);
 
